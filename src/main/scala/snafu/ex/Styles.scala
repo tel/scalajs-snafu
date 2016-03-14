5bba4878be7551b48c7d93813389988e984f00a2
Joseph Abrahamson <me@jspha.com>
2016-03-14 01:21:21 -0400

    Remove example styles, question applyToSelectorAll

--- src/main/scala/snafu/ex/Styles.scala
+++ src/main/scala/snafu/ex/Styles.scala
@@ -5,11 +5,11 @@ import org.scalajs.dom.{DOMList, Element}
 
 import scala.language.postfixOps
 import scalacss.Defaults._
-import scalacss.Percentage
 
 object Styles extends StyleSheet.Inline {
   import dsl._
 
+  // TODO: Should this go here? I highly doubt it, tbh.
   def applyToSelectorAll(sel: String, style: StyleA) {
     val els = dom.document.querySelectorAll(sel).asInstanceOf[DOMList[Element]]
     for ( ix <- 1 to els.length ) {
@@ -22,20 +22,4 @@ object Styles extends StyleSheet.Inline {
   val body = style(
     backgroundColor(c"#eee")
   )
-
-  val common = mixin(
-    backgroundColor.rgb(Percentage(95), Percentage(95), Percentage(95))
-  )
-
-  val outer = style(
-    common,
-    margin(12 px, auto),
-    textAlign.left,
-    cursor.pointer,
-
-    &.hover(
-      cursor.zoomIn
-    )
-  )
-
 }
